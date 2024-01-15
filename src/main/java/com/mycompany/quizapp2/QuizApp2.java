cc84e6617b4178dbdfaacac7d498ae2a1f541447
seigfred0 <saysonseigfred@gmail.com>
2024-01-15 18:59:28 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/quizapp2/QuizApp2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quizapp2;
+
+/**
+ *
+ * @author Seigfred
+ */
+public class QuizApp2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
